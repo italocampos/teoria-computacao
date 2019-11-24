c8a8a7007a5890472207987d219606405573a6eb
Italo Campos <italo.ramon.campos@gmail.com>
2019-11-24 01:37:31 -0300

    Adds some examples to use

--- pushdown_automata/tests.py
+++ pushdown_automata/tests.py
@@ -1,5 +1,38 @@
 from pdautomata import PDAutomata
 from tools import *
 
-G = read_from_file('grammar.in')
+# Exemplos de gramáticas pré-carregadas
+
+# Duplo balanceamento em {a, b}*
+G1 = {'V': ['S', 'B'],
+	'T': ['a', 'b'],
+	'R': [('S', 'aB'), ('S', 'aSB'), ('B', 'b')],
+	'S': 'S'}
+
+#  {wwr | w é palavra de {a, b}*}
+G2 = {'V': ['S', 'A', 'B'],
+	'T': ['a', 'b'],
+	'R': [('S', 'a'), ('S', 'b'), ('S', 'aSA'),
+		('S', 'bSB'), ('A', 'a'), ('B', 'b')],
+	'S': 'S'}
+
+# {wr | w é palavra de {a, b}*}
+G3 = {'V': ['S', 'A', 'B'],
+	'T': ['a', 'b'],
+	'R': [('S', 'a'), ('S', 'b'), ('S', 'aSA'),
+		('S', 'bSB'), ('S', 'aA'), ('S', 'bB'),
+		('A', 'a'), ('B', 'b')],
+	'S': 'S'}
+
+# Balanceamento de parêntesis
+G4 = {'V': ['S', 'A'],
+	'T': ['x', 'y', '(', ')'],
+	'R': [('S', '(SA'), ('S', 'xS'), ('S', 'yS'),
+		('S', 'x'), ('S', 'y'), ('S', '(A'), ('S', '(SAS'),
+		('S', 'xSS'), ('S', 'ySS'), ('S', '(AS'), ('A', ')')],
+	'S': 'S'}
+
+# Para ler uma nova gramática do arquivo, descomente a linha abaixo
+#G = read_from_file('grammar.in')
+
 auto = PDAutomata(G)
